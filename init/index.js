81ef80f6196c30d8d54277a18df16b0713283b30
Satyam Kumar <satyamkumar629301@gmail.com>
2026-02-17 20:22:21 +0530

    update initialize data

--- init/index.js
+++ init/index.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const initData = require("./data.js");
-const Job = require("../models/listingJob.js");
+import { connect } from "mongoose";
+import initData from "./data.js";
+import { deleteMany, insertMany } from "../models/listingJob.js";
 
 // connection with db
 main().then(()=>console.log("Connected to db")).catch((err)=>console.log(err));
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/listingjob");
+    await connect("mongodb://127.0.0.1:27017/listingjob");
 }
 
 // insert to db 
 const initDB = async () =>{
-    await Job.deleteMany({});
-    await Job.insertMany(initData);
+    await deleteMany({});
+    await insertMany(initData);
     console.log("Data is Added");
 };
 initDB();
