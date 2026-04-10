0510cdcf56f24efb8bd354567d18d2713b357286
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-10 12:11:17 +0530

    joi

--- schema.js
+++ schema.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 
+// will check at backend side if the data from frontend is valid before saving to db
+
 module.exports.jobSchema = Joi.object({
     listing : Joi.object({
         title : Joi.string().required(),
